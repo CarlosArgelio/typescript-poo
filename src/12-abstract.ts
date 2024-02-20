a49c538290b1ce8b7003c6e9cd8301745483da17
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-02-20 12:01:51 -0400

    add abstract'

---
+++ src/12-abstract.ts
@@ -0,0 +1,8 @@
+import { Animal, Dog  } from "./09-protected";
+
+// const animal = new Animal('elite');
+// animal.greeting();
+
+const Gar = new Dog('elite', 'Carlos');
+Gar.greeting();
+Gar.woof(2);
